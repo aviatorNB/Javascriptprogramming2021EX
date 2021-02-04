35e1cc3627e8912043f9a492a42ef2acdefcec68
aviatorNB <andrianovdp@gmail.com>
2021-02-04 11:26:36 -0400

    Initial commit

---
+++ concatenation-ex1.js
@@ -0,0 +1,21 @@
+// Standart comments
+
+let username = "Fred";
+const all = 'Web and Mobile Application development was the program ' + username + ' entered in the Fall of 2019. \n' +
+'Intro to Programming is going to be the course Fred likes best. ' + username + ' is going to study very hard. \n' +
+'Each night ' + username + ' is going to spend time reviewing all of the course material. \n' +
+username + ' will be committed to learning as much as possible about C# in 2019!';
+
+console.log(all);
+
+
+const allLiteralRepeat = `Web and Mobile Application development was the program ${username} entered in the Fall of 2019. \n` +
+`Intro to Programming is going to be the course Fred likes best. ${username} is going to study very hard. \n` +
+`Each night ${username} is going to spend time reviewing all of the course material. \n` +
+`${username} will be committed to learning as much as possible about C# in 2019!`;
+
+console.log(allLiteralRepeat);
+
+
+
+
